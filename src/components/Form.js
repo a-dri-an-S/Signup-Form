510388e149294f6b4ce707207986a88b4555ce34
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-01-06 21:31:59 -0800

    added form state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,41 @@
+import { useState } from "react";
+
 import IntroButton from "./IntroButton";
 
 const Form = () => {
+    const [firstName, setFirstName] = useState("First Name")
+    const [lastName, setLastName] = useState("Last Name")
+    const [email, setEmail] = useState("Email Address")
+    const [password, setPassword] = useState("Password")
+
     return (
         <section>
             <IntroButton />
             <form>
-                <input name="first name" value="First Name"/>
-                <input name="last name" value="Last Name"/>
-                <input name="email" value="Email Address"/>
-                <input name="password" value="Password"/>
+                <input
+                    type="text"
+                    name="first name" 
+                    value={firstName} 
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input 
+                    type="text"
+                    name="last name" 
+                    value={lastName} 
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                <input 
+                    type="email"
+                    name="email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input 
+                    type="password"
+                    name="password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <button>CLAIM YOUR FREE TRIAL</button>
             </form>
                 <p>By clicking the button, you are agreeing to our <span>Terms and Services</span></p>
